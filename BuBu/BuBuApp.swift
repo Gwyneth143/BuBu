ce4d23239e8076fa9d77079d81893de2c3aceafb
gwyneth <2858401629@qq.com>
2026-03-04 17:40:17 +0800

    Initial Commit

---
+++ BuBu/BuBuApp.swift
@@ -0,0 +1,17 @@
+//
+//  BuBuApp.swift
+//  BuBu
+//
+//  Created by Gwyneth on 2026/3/4.
+//
+
+import SwiftUI
+
+@main
+struct BuBuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
